6b1cbc3a79a2136f62a1a88b763fe93884d16c21
anoooop36 <anoooop36@gmail.com>
2020-07-08 22:20:14 +0530

    intersection point in y shaped linked lists

--- LinkedList/ReverseLinkedListInGroups.java
+++ LinkedList/ReverseLinkedListInGroups.java
@@ -1,3 +1,8 @@
+    /*
+    Given a linked list of size N. The task is to reverse every k nodes (where k is an input to the function) in the linked list.
+    https://practice.geeksforgeeks.org/problems/reverse-a-linked-list-in-groups-of-given-size/1
+    */
+    
     import java.util.*;
     import java.lang.*;
     
