f790359b187ad0ce47181ef3ab09ae5e48504506
anoooop36 <anoooop36@gmail.com>
2020-07-12 23:23:43 +0530

    vertical order traversal binary tree

---
+++ Tree/VerticalOrderTraversalOfBinaryTree.java
@@ -0,0 +1,166 @@
+/*
+Given a Binary Tree, print the vertical traversal of it starting from the leftmost level to the rightmost level.
+If there are multiple nodes passing through a vertical line, then they should be printed as they appear in level order traversal of the tree.
+https://practice.geeksforgeeks.org/problems/print-a-binary-tree-in-vertical-order/1
+*/
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class GfG {
+
+    static Node buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length)
+                break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    static void printInorder(Node root) {
+        if (root == null)
+            return;
+
+        printInorder(root.left);
+        System.out.print(root.data + " ");
+
+        printInorder(root.right);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            String s = br.readLine();
+            Node root = buildTree(s);
+            BinaryTree obj = new BinaryTree();
+            ArrayList<Integer> res = obj.verticalOrder(root);
+            for (Integer num : res)
+                System.out.print(num + " ");
+            System.out.println();
+
+        }
+    }
+}
+// } Driver Code Ends
+
+class Pair {
+    Node node;
+    int hd;
+
+    Pair(Node n, int d) {
+        node = n;
+        hd = d;
+    }
+}
+
+class BinaryTree {
+    /*
+     * Use level order traversal. Keep horizontal distances in along with node in
+     * queue. Need to maintain a list of numbers for each horizontal distance of
+     * tree in map. minHd and maxHd will help to print from leftmost to right most
+     */
+    static ArrayList<Integer> verticalOrder(Node root) {
+        ArrayList<Integer> res = new ArrayList<>();
+        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
+        int minHd = Integer.MAX_VALUE;
+        int maxHd = Integer.MIN_VALUE;
+
+        Queue<Pair> q = new LinkedList<Pair>();
+        q.add(new Pair(root, 0));
+
+        while (q.size() > 0) {
+
+            Pair currPair = q.poll();
+            int hd = currPair.hd;
+            Node node = currPair.node;
+            ArrayList<Integer> list = map.get(hd);
+            if (list == null) {
+                list = new ArrayList<>();
+            }
+            list.add(node.data);
+            map.put(hd, list);
+
+            if (node.left != null)
+                q.add(new Pair(node.left, hd - 1));
+            if (node.right != null)
+                q.add(new Pair(node.right, hd + 1));
+
+            if (minHd > hd)
+                minHd = hd;
+            if (maxHd < hd)
+                maxHd = hd;
+
+        }
+
+        for (int i = minHd; i <= maxHd; i++) {
+            res.addAll(map.get(i));
+        }
+        return res;
+    }
+}
